3035d9feafad84b72018da43ae2ef97c5eafea06
Serhii Turlak <moltenless@gmail.com>
2025-04-30 22:37:36 +0300

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeGame
+{
+    class Game
+    {
+        public bool[,] Matrix { get; set; }
+        //public int Size { get; }
+        public int Height { get; }
+        public int Width { get; }
+
+        public Game(int height, int width, Point[] initialLayout)
+        {
+            Matrix = new bool[height, width];
+            Height = height;
+            Width = width;
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                    Matrix[i, j] = initialLayout.Contains(new Point(i, j));
+        }
+
+        public void NextLayout()
+        {
+            bool[,] newMatrix = new bool[Height, Width]; 
+            for (int i = 0; i < Height; i++)
+                for (int j = 0; j < Width; j++)
+                {
+                    int count = GetNeighNum((i, j));
+                    if (Matrix[i, j] == true)
+                    {
+                        if (count == 2 || count == 3)
+                            newMatrix[i, j] = true;
+                        else if (count <= 1 || count >= 4)
+                            newMatrix[i, j] = false;
+                    }
+                    else if (Matrix[i, j] == false)
+                    {
+                        if (count == 3)
+                            newMatrix[i, j] = true;
+                        else
+                            newMatrix[i, j] = false;
+                    }
+                }
+            Matrix = newMatrix;
+        }
+
+        int GetNeighNum((int x, int y) coord)
+        {
+            int counter = 0;
+            for (int i = -1; i <= 1; i++)
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0) continue;
+                    if (coord.x + i == -1 || coord.x + i == Height ||
+                        coord.y + j == -1 || coord.y + j == Width)
+                        continue;
+                    if (Matrix[coord.x + i, coord.y + j])
+                    {
+                        counter++;
+                    }
+                }
+            return counter;
+        }
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LifeGame
+{
+    public partial class MainForm : Form
+    {
+        Game game;
+        int height;
+        int width;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            timer.Tick += timerTick;
+            SetStart();
+        }
+
+        void Draw()
+        {
+            Controls.Clear();
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                {
+                    Panel panel = new Panel
+                    {
+                        Size = new Size(20, 20),
+                        Location = new Point(j * 20, i * 20),
+                        BackColor = game.Matrix[i, j] ? Color.WhiteSmoke : Color.Black
+                    };
+                    Controls.Add(panel);
+                }
+        }
+
+        void Redraw()
+        {
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                {
+                    Controls[i * width + j].BackColor = game.Matrix[i, j] ? Color.WhiteSmoke : Color.Black;
+                }
+        }
+
+        Timer timer = new Timer { Interval = 300 };
+        bool paused = false;
+        private void MainForm_KeyUp(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                SetStart();
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+                if (!paused)
+                {
+                    timer.Stop();
+                    paused = true;
+                }
+                else
+                {
+                    timer.Start();
+                    paused = false;
+                }
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                if (timer.Interval >= 100)
+                    timer.Interval -= 50;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                if (timer.Interval <= 1400)
+                    timer.Interval += 50;
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
+        }
+
+        void timerTick(object sender, EventArgs e)
+        {
+            try
+            {
+                game.NextLayout();
+                Redraw();
+            }
+            catch
+            {
+                timer.Stop();
+                paused = true;
+            }
+        }
+
+        List<Point> points = new List<Point>();
+        bool pressed = false;
+        void SetStart()
+        {
+            this.KeyUp -= MainForm_KeyUp;
+            timer.Stop();
+            paused = true;
+            Controls.Clear();
+            points.Clear();
+            pressed = false;
+
+
+            height = 900 / 20;
+            width = 1550 / 20;
+
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                {
+                    Panel panel = new Panel
+                    {
+                        Size = new Size(20, 20),
+                        Location = new Point(j * 20, i * 20),
+                        BackColor = Color.DarkGray,
+                        Tag = new Point(i, j)
+                    };
+                    panel.MouseDown += (s2, e2) => { pressed = !pressed; };
+                    panel.MouseEnter += (s2, e2) =>
+                    {
+                        if (pressed)
+                        {
+                            (s2 as Panel).BackColor = Color.WhiteSmoke;
+                            points.Add((Point)(s2 as Panel).Tag);
+                        }
+                    };
+
+                    Controls.Add(panel);
+                }
+
+            void KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    Point[] layout = points.ToArray();
+
+                    Controls.Clear();
+                    this.KeyUp -= KeyDown;
+                    this.KeyUp += MainForm_KeyUp;
+                    game = new Game(height, width, layout);
+                    Draw();
+                    timer.Start();
+                    paused = false;
+                }
+                else if (e.KeyCode == Keys.Escape)
+                {
+                    Application.Exit();
+                }
+            }
+            this.KeyUp += KeyDown;
+        }
+
+    }
+}
+
